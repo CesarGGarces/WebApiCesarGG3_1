ebed5344a66364721fdebd8ae21674644a50450e
Cesar Garcia Garces <cesar-a@ra.mx>
2022-04-27 14:59:54 -0700

    carga inicial

---
+++ WebApiCesarGG3.1/Controllers/Ent.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCesarGG3._1.Model;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IoFile = System.IO.File;
+
+namespace WebApiCesarGG3._1.Controllers
+{
+    [Route("apiCesar/[controller]")]
+    [ApiController]
+    public class Ent : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public Ent(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+ 
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            try
+            {
+                var rutaExterna = _configuration.GetValue<string>("RutaExterna");
+            HttpClient cliente = new HttpClient();
+            var respuesta = await cliente.GetAsync(rutaExterna);
+            respuesta.EnsureSuccessStatusCode();
+            string ret = await respuesta.Content.ReadAsStringAsync();
+
+            EntJson? ent = JsonSerializer.Deserialize<EntJson>(ret);
+
+            var x = 0;
+            var cat0 = "";
+            var cat = "";
+            List<Distinta> distinta = new List<Distinta>();
+            foreach (var entrada in ent.entries)
+            {
+                cat0 = entrada.Category;
+                if (cat != cat0 && cat != "")
+                {
+                    distinta.Add(new Distinta() { Category = cat, Count = x });
+                    x = 0;
+                }
+                cat = entrada.Category;
+                x++;
+            }
+
+            string d = JsonSerializer.Serialize(distinta);
+
+            return d;
+            }
+            catch (Exception e)
+            {
+                IoFile.WriteAllText("LogApiCesar.txt", e.Message);
+                return e.Message;
+            }
+        }
+
+
+        [HttpGet("{val}")]
+        public async Task<string> GetAsync(bool val)
+        {
+            try { 
+            var rutaExterna = _configuration.GetValue<string>("RutaExterna");
+            HttpClient cliente = new HttpClient();
+            var respuesta = await cliente.GetAsync(rutaExterna);
+            respuesta.EnsureSuccessStatusCode();
+            string ret = await respuesta.Content.ReadAsStringAsync();
+
+            EntJson? ent = JsonSerializer.Deserialize<EntJson>(ret);
+
+            var q = from di in ent.entries
+                    where di.HTTPS == val
+                    select di;
+
+            List<Entrada> resHttps = new List<Entrada>();
+            foreach (var rHttps in q)
+            {
+                resHttps.Add(new Entrada()
+                {
+                    API = rHttps.API,
+                    Description = rHttps.Description,
+                    Auth = rHttps.Auth,
+                    HTTPS = rHttps.HTTPS,
+                    Cors = rHttps.Cors,
+                    Link = rHttps.Link,
+                    Category = rHttps.Category
+                });
+
+
+
+
+            }
+
+            string d = JsonSerializer.Serialize(resHttps);
+
+            return d;
+            }
+            catch (Exception e)
+            {
+                IoFile.WriteAllText("LogApiCesar.txt", e.Message);
+                return e.Message;
+            }
+        }
+
+
+    }
+}
---
+++ WebApiCesarGG3.1/Model/EntJson.cs
@@ -0,0 +1,28 @@
+﻿
+namespace WebApiCesarGG3._1.Model
+{
+    public class EntJson
+    {
+        public int count { get; set; }
+        public Entrada[] entries { get; set; }
+
+
+    }
+    public class Entrada
+    {
+        // public int count { get; set; }
+        public string API { get; set; }
+        public string Description { get; set; }
+        public string Auth { get; set; }
+        public bool HTTPS { get; set; }
+        public string Cors { get; set; }
+        public string Link { get; set; }
+        public string Category { get; set; }
+    }
+    public class Distinta
+    {
+        public string Category { get; set; }
+        public int Count { get; set; }
+
+    }
+}
